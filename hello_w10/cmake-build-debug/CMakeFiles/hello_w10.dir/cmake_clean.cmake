a0f81018441285a6d8e1910b3d9b5bbfe77d1baa
sunguanyong <1067946977@qq.com>
2021-07-28 17:47:20 +0800

    win10

---
+++ hello_w10/cmake-build-debug/CMakeFiles/hello_w10.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_w10.dir/main.cpp.obj"
+  "hello_w10.exe"
+  "hello_w10.exe.manifest"
+  "hello_w10.pdb"
+  "libhello_w10.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello_w10.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ hello_w10/cmake-build-debug/CMakeFiles/hello_w10.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for hello_w10.
+# This may be replaced when dependencies are built.
