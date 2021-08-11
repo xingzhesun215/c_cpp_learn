3f045456bb524a28f777f9a2d0e6aa93e125cd63
sun <1067946977@qq.com>
2021-08-12 00:07:04 +0800

    第一道题

--- helloworld/C++primerPlus习题/2-开始学习c++/2-1.cpp
+++ helloworld/C++primerPlus习题/2-开始学习c++/2-1.cpp
@@ -6,7 +6,7 @@ using namespace std;
  * 2.1 编写一个 c++的程序，用于显示你的姓名与地址
  * @return
  */
-int main() {
+int main1() {
     cout << "My name is xingzhesun215 , " << "and I am in Shenzhen" << endl;
     return 0;
 }
